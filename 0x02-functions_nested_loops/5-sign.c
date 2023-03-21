5565afae6b949ce48432575b79a19513b3eff080
Abdelrahman2335 <Abdelrahman.mostafa2335@gmail.com>
2023-03-21 13:54:58 -0700

    Project 0x02.C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * this is a function that prints sign of numbers
+ * if n greater than zero it will return: 1
+ * if n less than 1 it will return: -1
+ * if the n equal 0 it will return: 0
+ */
+
+
+int print_sign(int n)
+{
+	if(n < 0)
+	{
+		_putchar("-");
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else 
+	{
+		_putcahr("0");
+		return (0);
+	}
+}
