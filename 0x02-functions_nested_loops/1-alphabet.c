e2829587b610a6036a030a0941c7ea3ec27bac13
Abdelrahman2335 <Abdelrahman.mostafa2335@gmail.com>
2023-03-21 09:45:45 -0700

    Project 0x02.C

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,12 @@
  * print_alphabet - print all alphabet in lowercase
  */
 
-void print_alphabet(void) 
+void print_alphabet(void)
 {
+	char letter;
 
-	int i;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-	for(i = 'a' ; i <= 'z'; i++ )
-	{
-		_putchar(i);
-	}
 	_putchar('\n');
 }
