e0abd5cca9486326aaa73a25aceda21d261374d8
Abdelrahman2335 <Abdelrahman.mostafa2335@gmail.com>
2023-03-21 09:05:15 -0700

    Project 0x02.C

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
+ * prints the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
 {	
 	int i;
-	for (i="a"; i<="z")
+	for (i=0; i<=10)
 	{
 		_putchar(i);
 	}
