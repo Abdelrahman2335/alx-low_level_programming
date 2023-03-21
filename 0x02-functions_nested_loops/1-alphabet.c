d8a6b9e817a6963e253cce108eea9f4842b94846
Abdelrahman2335 <Abdelrahman.mostafa2335@gmail.com>
2023-03-21 08:49:31 -0700

    Project 0x02.C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+void print_alphabet(void)
+	
+	int i;
+
+	for(i="a"; i<="z"){
+		_putchar(i);
+	}
+	_putchar("\n");
