aa52db57ec56d109389d05af8b95a28813b6dcf2
Abdelrahman2335 <Abdelrahman.mostafa2335@gmail.com>
2023-03-21 14:02:46 -0700

    Project 0x02.C

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * this is a function that prints sign of numbers
- * if n greater than zero it will return: 1
- * if n less than 1 it will return: -1
- * if the n equal 0 it will return: 0
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 
 
 int print_sign(int n)
 {
-	if(n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
@@ -19,7 +20,7 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	}
+		}
 	else 
 	{
 		_putchar('0');
