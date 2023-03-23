7918137af30000d78d4baf6b24d8639d9e20faaa
Abdelrahman2335 <Abdelrahman.mostafa2335@gmail.com>
2023-03-22 22:29:32 -0700

    Project 0x03.C - Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
-* this code is testing that the function positive_or_negative()gutves the correct output when
-* given a case of 0
+* main - Test function for positive_or_negative
+* return: 0
 */
-
 
 int main(void)
 {
 	int i;
-
 	i = 0;
 	positive_or_negative(i);
-	
 	return (0);
 }
